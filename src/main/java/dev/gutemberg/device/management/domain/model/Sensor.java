3678030202fb10e5fcda9122fcd32018f7fcb4ca
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-05-26 10:03:02 -0300

    add missing id annotation

--- src/main/java/dev/gutemberg/device/management/domain/model/Sensor.java
+++ src/main/java/dev/gutemberg/device/management/domain/model/Sensor.java
@@ -3,6 +3,7 @@ package dev.gutemberg.device.management.domain.model;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Data
@@ -11,6 +12,7 @@ import lombok.*;
 @Builder
 @Entity
 public class Sensor {
+    @Id
     @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "BIGINT"))
     private SensorId id;
     private String name;
