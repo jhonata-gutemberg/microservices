8f544b495297a80654175d7d4625b2be5379e97f
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-01 06:40:55 -0300

    implementing temperature log controller

---
+++ src/main/java/dev/gutemberg/temperature/monitoring/domain/model/TemperatureLog.java
@@ -0,0 +1,23 @@
+package dev.gutemberg.temperature.monitoring.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.OffsetDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemperatureLog {
+    @Id
+    @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "uuid"))
+    private TemperatureLogId id;
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "sensor_id", columnDefinition = "bigint"))
+    private SensorId sensorId;
+    @Column(name = "\"value\"")
+    private Double value;
+    private OffsetDateTime registeredAt;
+}
