cfeeb05826dd97b81d6dd68de4e450031a57b436
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-02 10:00:07 -0300

    fix upsert

--- src/main/java/dev/gutemberg/temperature/monitoring/api/controller/SensorAlertController.java
+++ src/main/java/dev/gutemberg/temperature/monitoring/api/controller/SensorAlertController.java
@@ -6,7 +6,6 @@ import dev.gutemberg.temperature.monitoring.common.IdGenerator;
 import dev.gutemberg.temperature.monitoring.domain.model.SensorAlert;
 import dev.gutemberg.temperature.monitoring.domain.model.SensorId;
 import dev.gutemberg.temperature.monitoring.domain.repository.SensorAlertRepository;
-import dev.gutemberg.temperature.monitoring.domain.repository.SensorMonitoringRepository;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/api/sensors/{sensorId}/alert")
 @RequiredArgsConstructor
 public class SensorAlertController {
-    private SensorAlertRepository sensorAlertRepository;
+    private final SensorAlertRepository sensorAlertRepository;
 
     @GetMapping
     public SensorAlertOutput getOne(@PathVariable TSID sensorId) {
@@ -29,9 +28,8 @@ public class SensorAlertController {
     @PutMapping
     public SensorAlertOutput upsert(@PathVariable TSID sensorId, @RequestBody SensorAlertInput sensorAlertInput) {
         SensorAlert sensorAlert = sensorAlertRepository.findById(new SensorId(sensorId))
-                .orElse(SensorAlert.builder()
-                        .id(new SensorId(IdGenerator.generateTSID()))
-                        .build());
+                .orElse(new SensorAlert());
+        sensorAlert.setId(new SensorId(sensorId));
         sensorAlert.setMaxTemperature(sensorAlertInput.getMaxTemperature());
         sensorAlert.setMinTemperature(sensorAlertInput.getMinTemperature());
         sensorAlert = sensorAlertRepository.saveAndFlush(sensorAlert);
--- src/main/java/dev/gutemberg/temperature/monitoring/domain/model/SensorAlert.java
+++ src/main/java/dev/gutemberg/temperature/monitoring/domain/model/SensorAlert.java
@@ -3,6 +3,7 @@ package dev.gutemberg.temperature.monitoring.domain.model;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity
@@ -12,6 +13,7 @@ import lombok.*;
 @Builder
 public class SensorAlert {
     @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "bigint"))
+    @Id
     private SensorId id;
     private Double maxTemperature;
     private Double minTemperature;
