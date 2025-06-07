40c21d4ce6f3197d5236f1ec570f3de7cf653c2a
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-07 06:30:08 -0300

    simplify client with factory

--- src/main/java/dev/gutemberg/device/management/api/client/SensorMonitoringClientImpl.java
+++ src/main/java/dev/gutemberg/device/management/api/client/SensorMonitoringClientImpl.java
@@ -1,32 +1,15 @@
 package dev.gutemberg.device.management.api.client;
 
 import io.hypersistence.tsid.TSID;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     private final RestClient restClient;
 
-    public SensorMonitoringClientImpl(final RestClient.Builder builder) {
-        this.restClient = builder
-                .baseUrl("http://localhost:8082")
-                .requestFactory(temperatureMonitoringClientHttpRequestFactory())
-                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-                    throw new SensorMonitoringClientBadGatewayException();
-                })
-                .build();
-    }
-
-    public ClientHttpRequestFactory temperatureMonitoringClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(3));
-        factory.setReadTimeout(Duration.ofSeconds(5));
-        return factory;
+    public SensorMonitoringClientImpl(final SensorMonitoringRestClientFactory sensorMonitoringRestClientFactory) {
+        this.restClient = sensorMonitoringRestClientFactory.build();
     }
 
     @Override
---
+++ src/main/java/dev/gutemberg/device/management/api/client/SensorMonitoringRestClientFactory.java
@@ -0,0 +1,31 @@
+package dev.gutemberg.device.management.api.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class SensorMonitoringRestClientFactory {
+    private final RestClient.Builder builder;
+
+    public RestClient build() {
+        return builder.baseUrl("http://localhost:8082")
+                .requestFactory(clientHttpRequestFactory())
+                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
+                    throw new SensorMonitoringClientBadGatewayException();
+                })
+                .build();
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        return factory;
+    }
+}
