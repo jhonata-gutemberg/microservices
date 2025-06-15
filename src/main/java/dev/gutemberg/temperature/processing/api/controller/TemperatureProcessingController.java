b179891c09660ea8d60702db9311bcdd54268a4d
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-15 06:05:34 -0300

    send message to queue

--- src/main/java/dev/gutemberg/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/dev/gutemberg/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -3,25 +3,30 @@ package dev.gutemberg.temperature.processing.api.controller;
 import dev.gutemberg.temperature.processing.api.model.TemperatureLogOutput;
 import dev.gutemberg.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.time.OffsetDateTime;
+
+import static dev.gutemberg.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
         if (input == null || input.isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-
         Double temperature;
         try {
            temperature = Double.parseDouble(input);
@@ -35,6 +40,11 @@ public class TemperatureProcessingController {
                 .registeredAt(OffsetDateTime.now())
                 .build();
         log.info(temperatureLogOutput.toString());
+        MessagePostProcessor messagePostProcessor = (message) -> {
+            message.getMessageProperties().setHeader("sensorId", temperatureLogOutput.getSensorId().toString());
+            return message;
+        };
+        rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, "", temperatureLogOutput, messagePostProcessor);
     }
 
 }
