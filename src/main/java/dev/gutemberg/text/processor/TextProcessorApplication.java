de79e1f161a73b3a98e6bf4244609c36918fe72c
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-19 08:52:27 -0300

    init project

---
+++ src/main/java/dev/gutemberg/text/processor/TextProcessorApplication.java
@@ -0,0 +1,13 @@
+package dev.gutemberg.text.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorApplication.class, args);
+	}
+
+}
