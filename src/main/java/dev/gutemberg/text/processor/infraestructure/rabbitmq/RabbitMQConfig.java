8af098ffda588e38ec770df73ebf5451270df792
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-20 08:54:31 -0300

    create post-processing queue

---
+++ src/main/java/dev/gutemberg/text/processor/infraestructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package dev.gutemberg.text.processor.infraestructure.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String POST_PROCESSING_QUEUE = "text-processor-service.post-processing.v1.q";
+    public static final String POST_PROCESSING_EXCHANGE = "post-service.post-processing.v1.e";
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(final ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue postProcessingQueue() {
+        return QueueBuilder.durable(POST_PROCESSING_QUEUE).build();
+    }
+
+    @Bean
+    public Binding postProcessingBinding() {
+        return BindingBuilder.bind(postProcessingQueue()).to(postProcessingExchange());
+    }
+
+    public FanoutExchange postProcessingExchange() {
+        return ExchangeBuilder.fanoutExchange(POST_PROCESSING_EXCHANGE).build();
+    }
+}
