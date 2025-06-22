9a8c2ff884dbbcf82dfe4fbd803dbd333e011f48
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-22 08:19:32 -0300

    config json converter

--- src/main/java/dev/gutemberg/post/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/dev/gutemberg/post/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,8 +1,10 @@
 package dev.gutemberg.post.infrastructure.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,11 @@ public class RabbitMQConfig {
     @Bean
     public RabbitAdmin rabbitAdmin(final ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(final ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
