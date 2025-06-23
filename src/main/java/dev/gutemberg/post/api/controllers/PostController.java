9e2f6e8b79a2791df0708325be5f549cbda54689
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-23 08:38:25 -0300

    implementing post controller

---
+++ src/main/java/dev/gutemberg/post/api/controllers/PostController.java
@@ -0,0 +1,33 @@
+package dev.gutemberg.post.api.controllers;
+
+import dev.gutemberg.post.api.models.PostInput;
+import dev.gutemberg.post.api.models.PostOutput;
+import dev.gutemberg.post.domain.services.PostService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(final PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    PostOutput create(@RequestBody final PostInput postInput) {
+        return postService.create(postInput);
+    }
+
+    @GetMapping("/{postId}")
+    PostOutput getOne(@PathVariable final UUID postId) {
+        return postService.getOne(postId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+}
