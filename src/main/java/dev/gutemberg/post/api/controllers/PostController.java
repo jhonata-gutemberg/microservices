201d9e74c5166675a78d09f98f2e56a300b9f219
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-24 14:06:24 -0300

    add endpoint for search posts

--- src/main/java/dev/gutemberg/post/api/controllers/PostController.java
+++ src/main/java/dev/gutemberg/post/api/controllers/PostController.java
@@ -2,7 +2,11 @@ package dev.gutemberg.post.api.controllers;
 
 import dev.gutemberg.post.api.models.PostInput;
 import dev.gutemberg.post.api.models.PostOutput;
+import dev.gutemberg.post.api.models.PostSummaryOutput;
 import dev.gutemberg.post.domain.services.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.UUID;
@@ -29,5 +33,10 @@ public class PostController {
     PostOutput getOne(@PathVariable final UUID postId) {
         return postService.getOne(postId)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+
+    @GetMapping
+    Page<PostSummaryOutput> search(@PageableDefault final Pageable pageable) {
+        return postService.search(pageable);
     }
 }
