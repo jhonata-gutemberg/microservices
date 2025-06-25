ad4938374bc5aad7003ee038d21b45d680a8ac17
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-25 11:35:22 -0300

    remove api dependency on domain

--- src/main/java/dev/gutemberg/post/api/controllers/PostController.java
+++ src/main/java/dev/gutemberg/post/api/controllers/PostController.java
@@ -3,13 +3,16 @@ package dev.gutemberg.post.api.controllers;
 import dev.gutemberg.post.api.models.PostInput;
 import dev.gutemberg.post.api.models.PostOutput;
 import dev.gutemberg.post.api.models.PostSummaryOutput;
+import dev.gutemberg.post.domain.entities.Post;
 import dev.gutemberg.post.domain.services.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -17,6 +20,9 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+    private static final String LINE_SEPARATOR = "\n";
+    private static final int SUMMARY_LIMIT = 3;
+
     private final PostService postService;
 
     public PostController(final PostService postService) {
@@ -26,17 +32,50 @@ public class PostController {
     @PostMapping
     @ResponseStatus(CREATED)
     PostOutput create(@RequestBody final PostInput postInput) {
-        return postService.create(postInput);
+        var post = convertToEntity(postInput);
+        post = postService.create(post);
+        return convertToOutput(post);
     }
 
     @GetMapping("/{postId}")
     PostOutput getOne(@PathVariable final UUID postId) {
         return postService.getOne(postId)
+                .map(this::convertToOutput)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
     }
 
     @GetMapping
     Page<PostSummaryOutput> search(@PageableDefault final Pageable pageable) {
-        return postService.search(pageable);
+        return postService.search(pageable).map(this::summarize);
+    }
+
+    private Post convertToEntity(final PostInput postInput) {
+        return new Post(postInput.title(), postInput.body(), postInput.author());
+    }
+
+    private PostOutput convertToOutput(final Post post) {
+        return new PostOutput(
+                post.id(),
+                post.title(),
+                post.body(),
+                post.author(),
+                post.wordCount(),
+                post.calculatedValue()
+        );
+    }
+
+    private PostSummaryOutput summarize(final Post post) {
+        return new PostSummaryOutput(
+                post.id(),
+                post.title(),
+                buildSummary(post.body()),
+                post.author()
+        );
+    }
+
+    private String buildSummary(final String body) {
+        return Arrays.stream(body.splitWithDelimiters(LINE_SEPARATOR, SUMMARY_LIMIT + 1))
+                .limit(SUMMARY_LIMIT + 2L)
+                .collect(Collectors.joining());
     }
 }
--- src/main/java/dev/gutemberg/post/domain/services/PostService.java
+++ src/main/java/dev/gutemberg/post/domain/services/PostService.java
@@ -1,8 +1,5 @@
 package dev.gutemberg.post.domain.services;
 
-import dev.gutemberg.post.api.models.PostInput;
-import dev.gutemberg.post.api.models.PostOutput;
-import dev.gutemberg.post.api.models.PostSummaryOutput;
 import dev.gutemberg.post.domain.entities.Post;
 import dev.gutemberg.post.domain.models.PostProcessingRequest;
 import dev.gutemberg.post.domain.models.PostProcessingResult;
@@ -14,18 +11,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import static dev.gutemberg.post.infrastructure.rabbitmq.RabbitMQConfig.POST_PROCESSING_EXCHANGE;
 
 @Service
 public class PostService {
     private static final Logger LOGGER = LoggerFactory.getLogger(PostService.class);
-    private static final String LINE_SEPARATOR = "\n";
-    private static final int SUMMARY_LIMIT = 3;
 
     private final PostRepository postRepository;
     private final RabbitTemplate rabbitTemplate;
@@ -36,17 +29,15 @@ public class PostService {
     }
 
     @Transactional
-    public PostOutput create(final PostInput postInput) {
-        var post = new Post(postInput.title(), postInput.body(), postInput.author());
-        post = postRepository.saveAndFlush(post);
+    public Post create(Post post) {
         final var message = new PostProcessingRequest(post.id(), post.body());
         rabbitTemplate.convertAndSend(POST_PROCESSING_EXCHANGE, "", message);
-        return convert(post);
+        return postRepository.saveAndFlush(post);
     }
 
     @Transactional
-    public Optional<PostOutput> getOne(final UUID postId) {
-        return postRepository.findById(postId).map(this::convert);
+    public Optional<Post> getOne(final UUID postId) {
+        return postRepository.findById(postId);
     }
 
     @Transactional
@@ -63,33 +54,7 @@ public class PostService {
     }
 
     @Transactional
-    public Page<PostSummaryOutput> search(final Pageable pageable) {
-        return postRepository.findAll(pageable).map(this::summarize);
-    }
-
-    private PostOutput convert(final Post post) {
-        return new PostOutput(
-                post.id(),
-                post.title(),
-                post.body(),
-                post.author(),
-                post.wordCount(),
-                post.calculatedValue()
-        );
-    }
-
-    private PostSummaryOutput summarize(final Post post) {
-        return new PostSummaryOutput(
-                post.id(),
-                post.title(),
-                buildSummary(post.body()),
-                post.author()
-        );
-    }
-
-    private String buildSummary(final String body) {
-        return Arrays.stream(body.splitWithDelimiters(LINE_SEPARATOR, SUMMARY_LIMIT + 1))
-                .limit(SUMMARY_LIMIT + 2L)
-                .collect(Collectors.joining());
+    public Page<Post> search(final Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
 }
