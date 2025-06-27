96350b6c2ec99d47b8e37828e51d190341d3883d
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-06-27 16:27:47 -0300

    add createdAt and change body collumn definition

--- src/main/java/dev/gutemberg/post/domain/entities/Post.java
+++ src/main/java/dev/gutemberg/post/domain/entities/Post.java
@@ -1,10 +1,12 @@
 package dev.gutemberg.post.domain.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,16 +17,19 @@ public class Post {
     @Id
     private UUID id;
     private String title;
+    @Column(columnDefinition = "text")
     private String body;
     private String author;
     private Long wordCount;
     private BigDecimal calculatedValue;
+    private Instant createdAt;
 
     public Post(final String title, final String body, final String author) {
         this.id = UUID.randomUUID();
         this.title = title;
         this.body = body;
         this.author = author;
+        createdAt = Instant.now();
     }
 
     public Optional<Long> getWordCount() {
